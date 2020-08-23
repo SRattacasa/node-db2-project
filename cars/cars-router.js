f8007a154055bb7292ae6beab9c55b65c298b13d
SRattacasa <38533+SRattacasa@users.noreply.github.com>
2020-08-23 19:38:13 -0400

    Close to MVP

---
+++ cars/cars-router.js
@@ -0,0 +1,39 @@
+
+const db = require("../data/config")
+const express = require("express")
+
+const router = express.Router()
+
+
+
+router.get('/cars', async (req, res) => { 
+    try {
+    const accountList = await db.select("*").from("cars")
+    res.status(200).json(accountList)
+    } catch (err) {
+        next(err)
+    }
+})
+
+
+
+router.post("/cars",   async (req, res) => { 
+    try {
+        const id = await db.insert({
+            make: req.body.make,
+            model: req.body.model,
+            VIN: req.body.VIN,
+            mileage: req.body.mileage,   
+        })
+        .into("cars")
+        
+        const newAcct = await db("cars").where("id", id)
+        
+        res.status(201).json(newAcct)
+        } catch (err) {
+            next(err) 
+        }
+})
+
+
+module.exports = router
